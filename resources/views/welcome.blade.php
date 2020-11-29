1936cbe01e1455930071547f421ece1ffae84314
Alex Barber <alex.barber@fifteen.co.uk>
2020-11-29 22:32:35 +0000

    started work on Known coding languages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,24 @@
 @include('site-wide.home-hero')
 
 
+<section id="languages-section">
 
+    <div class="container">
+
+        <div class=" row">
+            <div class="languages-content col-md-2">
+                <div class="languages-image" style="background-image: url('<?php echo url("/") ?>/images/css-logo.jpg')"></div>
+            </div>
+
+
+
+        </div>
+
+
+
+    </div>
+
+</section>
 
 
 
